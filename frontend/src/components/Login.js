7b8d2b9eecbd34a22b3163e8a1d4d3d7f6c48818
Aniket862003 <127227833+Aniket862003@users.noreply.github.com>
2025-08-18 23:34:24 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import API_BASE_URL from "../config";
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
@@ -14,7 +15,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "https://foodweb-backend-g881.onrender.com/api/auth/login",
+         `${API_BASE_URL}/api/auth/login`,
         formData
       );
 
